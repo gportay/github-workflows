3f9dedc7a44ddaf968f5a454918ec1ec6be5edc8
Gaël PORTAY <gael.portay@rtone.fr>
2025-07-24 06:55:47 +0200

    make-deb.sh: bind-mount home directory

--- make-deb.sh
+++ make-deb.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S DOSH_DOCKERFILE=Dockerfile.deb dosh
+#!/usr/bin/env -S DOSH_DOCKERFILE=Dockerfile.deb home=1 dosh
 set -e
 dpkg-buildpackage -us -uc "$@"
 lintian ../github-workflows*.dsc ../github-workflows*.deb
